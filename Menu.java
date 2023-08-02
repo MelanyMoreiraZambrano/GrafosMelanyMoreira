ebc7d430380d339e048dd3770caf0c77615d1f87
Melany Moreira <139522731+MelanyMoreiraZambrano@users.noreply.github.com>
2023-08-01 21:02:39 -0500

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,110 @@
+package grafos;
+import java.util.List;
+import java.util.Scanner;
+public class Menu {
+    int contV= 0;
+    String nombre;
+    //--------------------------------------------------------
+    //Método de la clase Menu, que me permite ejecutar el menú de opciones para 
+    //usar sus operaciones básicas
+    //--------------------------------------------------------
+    public void menu(GrafoMatriz grafo) {
+        Scanner sc = new Scanner(System.in);
+        menuInfo();
+        int opcion = sc.nextInt();
+        sc.nextLine();
+        switch (opcion) {
+            case 1:
+                System.out.println("------------------------------------------------");
+                System.out.println("          Creacion de vertices (nodos)");
+                System.out.println("------------------------------------------------");
+                System.out.println("Ingrese cuantos vertices va a crear:");
+                int nVertices = sc.nextInt();
+                sc.nextLine(); // Consumimos el salto de línea después de leer el entero.
+                int totalVertices = nVertices + contV;
+                if (nVertices < 1) {
+                    System.out.println("Lo sentimos no puede ingresar un numero negativo:");                    
+                } else {
+                    if (totalVertices > 20) {
+                        System.out.println("Lo sentimos, la cantidad que usted ingresa sobrepasa los vertices totales permitidos");                       
+                    } else {
+                        for (int i = 0; i < nVertices; i++) {
+                            System.out.println("------------------------------------------------");
+                            System.out.println("            Creacion del grafo numero ");
+                            System.out.println("------------------------------------------------");
+                            System.out.println("Ingrese el nombre de referencia para el vertice:");
+                            nombre = sc.nextLine();
+                            grafo.nuevoVertice(nombre.trim());
+                            System.out.println("-----------------Se creo correctamente-----------");
+                            contV = contV + 1;
+                        }                        
+                    }
+                }
+                menu(grafo);
+                break;
+            case 2:
+                System.out.println("------------------------------------------------");
+                System.out.println("            Conectar vertices ");
+                System.out.println("------------------------------------------------");
+                System.out.println("Ingrese el nombre del vertice a conectar:");
+                String vertice1 = sc.nextLine();
+                System.out.println("Ingrese el nombre del vertice a conectar con " + vertice1);
+                String vertice2 = sc.nextLine();
+                try {
+                    grafo.nuevoArco(vertice1, vertice2);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                menu(grafo);
+                break;
+            case 3:
+                System.out.println("------------------------------------------------");
+                System.out.println("      Ver si un vertice es adyacente con otro");
+                System.out.println("------------------------------------------------");
+                System.out.println("Ingrese el nombre del vertice a verificar:");
+                String vertice = sc.nextLine();
+                System.out.println("Ingrese el nombre del vertice a verificar con:" + vertice);
+                String verticeVerificar = sc.nextLine();
+                try {
+                    System.out.println(grafo.adyacente(vertice, verticeVerificar));                  
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                menu(grafo);
+                break;
+            case 4:
+                System.out.println("------------------------------------------------");
+                System.out.println("     Mostrar las adyacencias de un vertice");
+                System.out.println("------------------------------------------------");
+                System.out.println("Ingrese el nombre del vertice a buscar sus adyacencias:");
+                String verticeBuscar = sc.nextLine();
+                try {
+                    List<Vertice> nodosConectadosA = grafo.nodosConectados(verticeBuscar);
+                    System.out.println("Nodos conectados a ("+verticeBuscar+")");
+                    for (Vertice v : nodosConectadosA) {
+                        System.out.println(v.nomVertice());
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+                menu(grafo);
+            	break;
+            case 5:
+                System.out.println("------------------------------------------------");
+                System.out.println("           Muchas gracias por su visita");
+                System.out.println("------------------------------------------------");
+            	break;
+        }
+    }
+    public void menuInfo () {
+        System.out.println("------------------------------------------------");
+        System.out.println("            Menu principal de grafos");
+        System.out.println("------------------------------------------------");
+        System.out.println("Ingrese 1 Ingresar nodos nuevos ( Max 20 )");
+        System.out.println("Ingrese 2 Conectar 2 vertices");
+        System.out.println("Ingrese 3 Ver si un vertice es adyacente con otro");
+        System.out.println("Ingrese 4 Mostrar las adyacencias de un vertice");        
+        System.out.println("Ingrese 5 Salir");
+        System.out.println("------------------------------------------------");
+    }
+}
